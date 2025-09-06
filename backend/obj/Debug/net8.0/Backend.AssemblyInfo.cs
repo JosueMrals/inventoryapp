d2b87e9043f441fb7229230288b6b1e4dae51408
JosueMrals <josuemral10@gmail.com>
2025-09-05 23:05:28 -0600

    --fix: Migration Bar code and Description

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01a691e4b4e85f03982296eb350bbb1ee29c1c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54095048848ed034d36c76c4bcb236490402204d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
