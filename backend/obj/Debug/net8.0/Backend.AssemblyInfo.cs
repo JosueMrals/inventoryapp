3abeb50d45eecad4de0bbbf4b4c110232d4cb967
JosueMrals <josuemral10@gmail.com>
2025-10-22 01:13:27 -0600

    --update: Change backend url Render v2.0

--- backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43047a328bee658a9f1b99780bada376e7e4d4eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffd99fb4dd419b97d324313dc670d202ffd55464")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
